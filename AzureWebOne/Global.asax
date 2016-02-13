598a1f74836f0db39164df5a89d01571b47eb1e9
Grant <GrantNWade@gmail.com>
2016-02-13 11:28:16 -0600

    Initial Commit

---
+++ AzureWebOne/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AzureWebOne.MvcApplication" Language="C#" %>
---
+++ AzureWebOne/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzureWebOne.Startup))]
+namespace AzureWebOne
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
